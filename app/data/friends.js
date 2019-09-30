325e1e60346f11f6a947e6ffaab23fcd5f95c007
Kevin Malone <zhaokevinma@gmail.com>
2019-09-29 21:20:08 -0400

    Asynchronous issue

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 // data
 var friendsArray = [
     {
-      "name": "Pikachu",
-      "photo":"https://images.app.goo.gl/L4zg1GyrHEEQRZkg8",
-      "scores":[
+      name: "Pikachu",
+      photo:"https://images.app.goo.gl/L4zg1GyrHEEQRZkg8",
+      scores:[
           5,
           5,
           5,
@@ -14,6 +14,22 @@ var friendsArray = [
           5,
           5,
           5
+      ]
+    },
+    {
+      name: "Spongebob",
+      photo:"https://images.app.goo.gl/faue2ffs7Wqphb5KA",
+      scores:[
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1
       ]
     }
   ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,7 +11,29 @@ module.exports = function(app) {
 
   // post
   app.post("/api/friends", function(req, res) {
+
+    // push to data/friend.js
     friendsData.push(req.body);
+
+    // diff array
+    var diffArray = [];
+    
+    // loop through data and push diff into diff array
+    for (let i = 0; i < friendsData.length - 1; i++) {
+      var diff = 0;
+      for (let j = 0; j < friendsData[i].scores.length; j++) {
+        if (friendsData[i].scores[j] < friendsData[friendsData.length-1].scores[j]) {
+          diff += friendsData[friendsData.length-1].scores[j] - friendsData[i].scores[j];
+        } else {
+          diff += friendsData[i].scores[j] - friendsData[friendsData.length-1].scores[j];
+        }
+      }
+      diffArray.push(diff);
+    }
+
+    var indexOfMin = diffArray.indexOf(Math.min(diffArray));
+
+    res.json(friendsData[indexOfMin]);
   });
 
 };
