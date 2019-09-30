dbdd527abb3b1246895972e754a496ed6689dedb
Kevin Malone <zhaokevinma@gmail.com>
2019-09-30 13:21:32 -0400

    survey page needs re-work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,18 @@
+// data
 var friends = require("../data/friends");
 
 module.exports = function(app) {
-  // Return all friends found in friends.js as JSON
+  // get
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
+  // post
   app.post("/api/friends", function(req, res) {
+    // test purposes can delete
     console.log(req.body.scores);
 
-    // Receive user details (name, photo, scores)
+    // store request body
     var user = req.body;
 
     // parseInt for scores
@@ -17,12 +20,11 @@ module.exports = function(app) {
       user.scores[i] = parseInt(user.scores[i]);
     }
 
-    // default friend match is the first friend but result will be whoever has the minimum difference in scores
+    // default match index and diff
     var bestFriendIndex = 0;
     var minimumDifference = 40;
 
-    // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
-    //  whatever the difference is, add to the total difference
+    //  easy way to loop and compare but can improve on run-time using other methods
     for(var i = 0; i < friends.length; i++) {
       var totalDifference = 0;
       for(var j = 0; j < friends[i].scores.length; j++) {
@@ -30,17 +32,17 @@ module.exports = function(app) {
         totalDifference += difference;
       }
 
-      // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+      // update index and diff if condition meets
       if(totalDifference < minimumDifference) {
         bestFriendIndex = i;
         minimumDifference = totalDifference;
       }
     }
 
-    // after finding match, add user to friend array
+    // push to data array although this is not needed if no database for storage of info 
     friends.push(user);
 
-    // send back to browser the best friend match
+    // send back best match as a json -> modal needs for display
     res.json(friends[bestFriendIndex]);
   });
 };
